83c5ecb0b5f76af8af929228533ef5a241928f87
jhoannr678-spec <jhoannr678@gmail.com>
2026-07-25 00:16:29 -0500

    Update preguntas.js

--- preguntas.js
+++ preguntas.js
@@ -1,1 +1,112 @@
+const preguntas = [
 
+{
+pregunta:"¿Cuál es la misión principal de Skechers?",
+opciones:[
+"Vender únicamente zapatos deportivos",
+"Ayudar a los consumidores a lucir y sentirse bien mediante productos cómodos, innovadores y de calidad",
+"Fabricar ropa de lujo",
+"Ser la empresa más barata del mundo"
+],
+correcta:1
+},
+
+{
+pregunta:"¿Cuál es la visión de Skechers?",
+opciones:[
+"Ser líder únicamente en Colombia",
+"Ser la primera opción de compra ofreciendo una experiencia excepcional para toda la familia",
+"Fabricar únicamente tenis",
+"Expandirse solo en América"
+],
+correcta:1
+},
+
+{
+pregunta:"¿Cuál de estos es un atributo de Skechers?",
+opciones:[
+"Comodidad",
+"Comida rápida",
+"Telefonía",
+"Electrónica"
+],
+correcta:0
+},
+
+{
+pregunta:"¿Qué tecnología caracteriza a Skechers?",
+opciones:[
+"Memory Foam",
+"Windows",
+"Bluetooth",
+"Intel"
+],
+correcta:0
+},
+
+{
+pregunta:"¿Qué significa 'Magia con una Sonrisa'?",
+opciones:[
+"Atender con una sonrisa genuina y entusiasmo",
+"Vender rápido",
+"No hablar con el cliente",
+"Esperar a que el cliente pregunte"
+],
+correcta:0
+},
+
+{
+pregunta:"Una competencia esencial es:",
+opciones:[
+"Orientación a resultados",
+"Dormir en el trabajo",
+"Improvisar siempre",
+"Competir con los compañeros"
+],
+correcta:0
+},
+
+{
+pregunta:"Antes de entregar un producto debes:",
+opciones:[
+"Revisar talla y referencia",
+"Entregarlo sin verificar",
+"Preguntar a otro cliente",
+"Guardarlo nuevamente"
+],
+correcta:0
+},
+
+{
+pregunta:"La comunicación en Skechers debe ser:",
+opciones:[
+"Clara e inclusiva",
+"Solo por mensajes",
+"Únicamente del jefe",
+"Ninguna de las anteriores"
+],
+correcta:0
+},
+
+{
+pregunta:"La flexibilidad significa:",
+opciones:[
+"Aceptar el cambio",
+"Evitar aprender",
+"No colaborar",
+"Ignorar los procesos"
+],
+correcta:0
+},
+
+{
+pregunta:"La excelencia en producto busca:",
+opciones:[
+"Mostrar la marca de forma atractiva",
+"Esconder el producto",
+"Desordenar la tienda",
+"Reducir el servicio"
+],
+correcta:0
+}
+];
